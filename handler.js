58c78bf51ce93cd4330441b04fb98db6962899c6
BrandtBasaran <100269878+BrandtBasaran@users.noreply.github.com>
2022-08-20 00:46:05 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,15 +1,19 @@
-"use strict";
+'use strict'
+const AWS = require('aws-sdk')
 
-module.exports.hello = async (event) => {
+module.exports.taskManager = async (event) => {
+  const body = JSON.parse(Buffer.from(event.body, 'base64').toString())
+  const dynamoDb = new AWS.DynamoDB.DocumentClient()
+  const putParams = {
+    TableName: process.env.DYNAMODB_TASK_MANAGER,
+    Item: {
+      primary_key: body.name,
+      email: body.email
+    }
+  }
+  await dynamoDb.put(putParams).promise()
+
   return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-};
+    statusCode: 201
+  }
+}
