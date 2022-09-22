7b96daedbe8d391987d94f26a3081f02e25ab643
dana.bradbury <dana.bradbury@autofinance.com>
2022-09-22 15:39:09 -0400

    fixed the error response

--- source/post-task.js
+++ source/post-task.js
@@ -2,39 +2,34 @@ const AWS = require("aws-sdk");
 
 module.exports.handler = async (event) => {
   const response = { statusCode: 401 };
-
-  // validate the request
-  const body = JSON.parse(event.body);
-
-  console.log(body);
-  const dynamoDb = new AWS.DynamoDB.DocumentClient({ region: "us-east-1" });
-  const now = new Date();
-  console.log(now.toISOString());
-  const pk = `dummy hard coded PK ${now.toISOString()}`;
-  const putParams = {
-    TableName: process.env.DYNAMODB_TASK_TABLE,
-    Item: {
-      primary_key: pk,
-      sort_key: `dummy hard coded SK`,
-    },
-  };
-
-  console.log(JSON.stringify(putParams));
   try {
-    // this request is valid, now do something with it.
-    throw new Error("bad shit");
-    await dynamoDb.put(putParams).promise();
+    // validate the request
+    const body = JSON.parse(event.body);
+    console.log(body);
 
+    // this request is valid, now do something with it.
+    const dynamoDb = new AWS.DynamoDB.DocumentClient({ region: "us-east-1" });
+    const now = new Date();
+    console.log(now.toISOString());
+    const pk = `dummy hard coded PK ${now.toISOString()}`;
+    const putParams = {
+      TableName: process.env.DYNAMODB_TASK_TABLE,
+      Item: {
+        primary_key: pk,
+        sort_key: `dummy hard coded SK`,
+      },
+    };
+
+    console.log(JSON.stringify(putParams));
+    await dynamoDb.put(putParams).promise();
     // it was a valid request, I did something with it, and everything worked as expected
     response.statusCode = 200;
   } catch (error) {
     response.statusCode = 500;
-    //console.log(error.message);
-    //console.log(error);
-    //response.body = JSON.stringify(error);
-    //console.log(error);
-
-    const body = { message: "we made a mistake, come back later"};
+    const body = {
+      message: "we made a mistake, come back later",
+      details: error.message,
+    };
     response.body = JSON.stringify(body);
   }
 
