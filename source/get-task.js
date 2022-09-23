9cec902e4609f80627269183927c54b32ac8dee7
BrandtBasaran <brandtbasaran@gmail.com>
2022-09-23 05:38:53 -0400

    practice round on id verified getrequest

--- source/get-task.js
+++ source/get-task.js
@@ -1,61 +1,55 @@
 "use strict";
 
 const handler = async (event) => {
-  
 
-  try {
-// get the id from the request
-    //we need to get userID from the get request. we changed serverless yaml to accept an ID as a string, just not sure the proper code to retrieve that. just filled w bull shit below for now
-    var userId = getTask.handler{id};
-
-    // validate the id is there and is a valid number
-
-    if(isNaN(userId)){
-	    const response = {
-        statusCode: 400,
-        body: JSON.stringify(
-          {
-            message: "ID invalid, not a number"
-          }
-      }
-   }else{
-    // if the id is valid, go tot he database and get some shit
-    const results = await dynamoDb.get(putParams).promise();
-    // inspect the result from dynamo, if found, build the response with a 200
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: "Get got!",
-        },
-        null,
-        2
-      ),
-    };
-   }
-    
-
-    
-        // if missing or not a number, set a 400 status and return
-
-    
-      
-
-        // if the results from the db are empty, set a 404 not found and return the empty response
-  
-    
-    // it was a valid request, I did something with it, and everything worked as expected
-    response.statusCode = 200;
-  } catch (error) {
-    response.statusCode = 500;
-    response.body = JSON.stringify(error);
-    console.error(error);
-  }
-
-  // return
-  return response;
+    function checkRequestId(userId) {
+        const requestId = req.path[1];
+        //path for get task currently is /task/{id}, so we pull the request id from req.path[1]
+        try {
+            if (isNaN(userId)) {
+                //if userId isNaN we return a 400 status to signal input error on user side
+                const response = {
+                    statusCode: 400,
+                    body: JSON.stringify(
+                        {
+                            message: "ID invalid, not a number"
+                        }
+                    )
+                }
+            } else if (!isNaN(userId) && userId != req.path[1]) {
+              //if user Id isNaN and != req.path[1] we return a 404 because it is not in our records 
+              const response = {
+                    statusCode: 404,
+                    body: JSON.stringify({
+                        message: "ID was not found in our records"
+                    })
+                }
+            } else {
+                // if the id is valid, go tot he database and get some shit. this result object needs work as well
+                const results = await dynamoDb.getItem(getParams).promise();
+                // inspect the result from dynamo, if found, build the response with a 200
+                const response = {
+                    statusCode: 200,
+                    body: JSON.stringify(
+                        {
+                            message: "Get got!",
+                        },
+                        null,
+                        2
+                    ),
+                };
+            }
+        }
+        catch (error) {
+            response.statusCode = 500;
+            response.body = JSON.stringify(error);
+            console.error(error);
+        }
+    }
+    // return
+    return response;
 };
 
 module.exports = {
-  handler,
+    handler,
 };
